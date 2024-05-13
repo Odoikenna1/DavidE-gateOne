a4dda488886f0f9b5eda5ae17c1e2a4b6078937b
unknown <davidodojunior@gmail.com>
2024-05-13 11:03:49 -0700

    updated

--- DaySeven/Task1/ATM Machine App/Java/Task7.java
+++ DaySeven/Task1/ATM Machine App/Java/Task7.java
@@ -1,9 +1,12 @@
+
 import java.util.*;
 
 public class Task7
 {
 	public static void main(String[] args)
 	{
+
+		//ArrayList<ArrayList<Object>> customerData = new ArrayList<ArrayList<Object>>();
 
 		String message = """
 		\nWelcome to Semicolon ATM services.
@@ -14,17 +17,26 @@ public class Task7
 		5 -> Transfer
 		""";
 
-		Object[][] customerData = {
-			{null, null, null}, //customer names
-			{500, 850, 340} //account balance
-		};
+		Object[][] customerData = new Object [2][3];	//2 columns; 1 for account number and balance
+
+		/*
+		customerData[0][0] first array row is for customer name
+		customerData[1][0] first array row is for account number
+		customerData[2][0] first array row is for account balance
+		*/
 		
 		int rows = customerData.length;
 		int columns = customerData[0].length;
 		
-		double accountBalance = 0.00;
+		double availableBalance = 0.00;
+		double ledgerBalance = 0.00;
 
 		System.out.print(message);
+
+		Random variantAccountNumber = new Random();
+		long newUserAccountNumber = Math.abs(variantAccountNumber.nextLong());
+		
+		System.out.println(newUserAccountNumber);
 
 		Scanner input = new Scanner(System.in);
 	
@@ -41,11 +53,17 @@ public class Task7
 				System.out.print(createAccountOptions + "\nWhat is your name?  ");
 				String customerName = input.nextLine();
 				customerData[0][0] = customerName;
+				customerData[1][0] = newUserAccountNumber;
+			break;
+			case 2:
+				System.out.print("Enter your account number: ");
+				System.out.print("Available Balance: " + availableBalance);
+				System.out.print("Ledger Balance: " + availableBalance);
 			break;
 		}
 		System.out.println();
-		System.out.printf("%8s Account balance%.2f%n", "", accountBalance, "");
-		System.out.println(customerData[0][0]);
+		System.out.printf("%8s Account number %8s Accountbalance%n%n", "","");
+		System.out.printf("%s%8s%.2f%8s%s%n", customerData[0][0], "", availableBalance, "", customerData[1][0]);
 
 
 
