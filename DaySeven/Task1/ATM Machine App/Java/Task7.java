060f06f852b5b1fc9148e6f42d2a41a46d8522c7
unknown <davidodojunior@gmail.com>
2024-05-13 07:02:19 -0700

    Day seven, first draft

---
+++ DaySeven/Task1/ATM Machine App/Java/Task7.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Task7
+{
+	public static void main(String[] args)
+	{
+
+		String message = """
+		\nWelcome to Semicolon Bank Customer.
+		What would you like to do today?
+		1 -> Create New Account     2 -> Check Balance
+		3 -> Deposit Money	    4 -> Withdraw Funds
+		5 -> Transfer
+		""";
+
+		String[] customerName = new customerName[3];
+		Int[][]
+		
+		int rows = customerData.length;
+		int columns = customerData[0].length;
+
+		System.out.println(rows);
+		System.out.println(columns);
+
+		//System.out.print(message);
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Please enter Your name: ");
+		String userInput = input.nextLine();
+
+		customerData[0][0] = userInput;
+	}
+}
