066f5ceae6567cb0c7a087aa4302a900f9fbe9ea
unknown <davidodojunior@gmail.com>
2024-05-13 13:20:22 -0700

    updated

---
+++ DaySeven/Task1/ATM Machine App/Java/Functions.java
@@ -0,0 +1,44 @@
+import java.security.SecureRandom;
+import java.util.*;
+public class Functions
+{
+	public static int createAccount(int menuInput)
+	{
+		Scanner input = new Scanner(System.in);
+
+		Object[][] customerData = new Object [4][3];
+		double availableBalance = 0.00;
+
+		SecureRandom newUserAccountNumber = new SecureRandom();
+
+		long accountNumber = newUserAccountNumber.nextLong(1000000000, 1111111111);
+
+		int rows = customerData.length;
+		int columns = customerData[0].length;
+		
+
+		double ledgerBalance = 0.00;
+
+		switch(menuInput)
+		{
+			case 1:
+				//input.nextLine();
+				System.out.print("\nWhat is your name?  ");
+				String customerName = input.nextLine();
+				customerData[0][0] = customerName;
+				customerData[0][1] = accountNumber;
+				customerData[0][2] = availableBalance;
+				System.out.println("\nYour account was successfully created.\nYour account number is " + customerData[0][1]);
+				System.out.print("\nYou are required to fund your account within the next 24hrs, or risk termination of your account.\nTo Fund your account now, select the deposit money option in the main menu.\n");
+
+				System.out.println("\nPress 0 to return to main menu or # to exit: \n");
+				String userInputForExitOrReturn = input.nextLine();
+			break;
+			default:
+				System.out.println("Invalid input\nTry again.");
+		}
+		System.out.println();
+		System.out.printf("%n%8s Account number %6s Account balance%n%n", "","");
+		System.out.printf("%s%8s%.2f%19s%d%n", customerData[0][0], "", availableBalance, "", customerData[1][0]);
+	} 
+}
--- DaySeven/Task1/ATM Machine App/Java/Task7.java
+++ DaySeven/Task1/ATM Machine App/Java/Task7.java
@@ -17,58 +17,15 @@ public class Task7
 		5 -> Transfer
 		""";
 
-		Object[][] customerData = new Object [2][3];	//2 columns; 1 for account number and balance
-
-		/*
-		customerData[0][0] first array row is for customer name
-		customerData[1][0] first array row is for account number
-		customerData[2][0] first array row is for account balance
-		*/
-		
-		int rows = customerData.length;
-		int columns = customerData[0].length;
-		
-		double availableBalance = 0.00;
-		double ledgerBalance = 0.00;
-
 		System.out.print(message);
-
-		Random variantAccountNumber = new Random();
-		long newUserAccountNumber = Math.abs(variantAccountNumber.nextLong());
-		
-		System.out.println(newUserAccountNumber);
 
 		Scanner input = new Scanner(System.in);
 	
 		System.out.print("\nEnter an option: ");
 		int userOption = input.nextInt();
-		
-		switch(userOption)
-		{
-			case 1:
-				String createAccountOptions = """
-				\nWe need the following details to create your new account.
-				""";
-				input.nextLine();
-				System.out.print(createAccountOptions + "\nWhat is your name?  ");
-				String customerName = input.nextLine();
-				customerData[0][0] = customerName;
-				customerData[1][0] = newUserAccountNumber;
-			break;
-			case 2:
-				System.out.print("Enter your account number: ");
-				System.out.print("Available Balance: " + availableBalance);
-				System.out.print("Ledger Balance: " + availableBalance);
-			break;
-		}
-		System.out.println();
-		System.out.printf("%8s Account number %8s Accountbalance%n%n", "","");
-		System.out.printf("%s%8s%.2f%8s%s%n", customerData[0][0], "", availableBalance, "", customerData[1][0]);
 
+		int accountCreated = Functions.createAccount(userOption);
 
-
-
-
-
+		System.out.print(accountCreated);
 	}
 }
