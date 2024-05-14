4299e871a02596c52edb4c66792bf56fe3eb02d2
unknown <davidodojunior@gmail.com>
2024-05-13 23:38:33 -0700

    still thinking

--- DaySeven/Task1/ATM Machine App/Java/Functions.java
+++ DaySeven/Task1/ATM Machine App/Java/Functions.java
@@ -6,40 +6,6 @@ public class Functions
 	{
 		Scanner input = new Scanner(System.in);
 
-		Object[][] customerData = new Object [4][3];
-		double availableBalance = 0.00;
-
-		SecureRandom newUserAccountNumber = new SecureRandom();
-
-		long accountNumber = newUserAccountNumber.nextLong(1000000000, 1111111111);
 		
-
-		int rows = customerData.length;
-		int columns = customerData[0].length;
-		
-
-		double ledgerBalance = 0.00;
-
-		switch(menuInput)
-		{
-			case 1:
-				//input.nextLine();
-				System.out.print("\nWhat is your name?  ");
-				String customerName = input.nextLine();
-				customerData[0][0] = customerName;
-				customerData[0][1] = accountNumber;
-				customerData[0][2] = availableBalance;
-				System.out.println("\nYour account was successfully created.\nYour account number is " + customerData[0][1]);
-				System.out.print("\nYou are required to fund your account within the next 24hrs, or risk termination of your account.\nTo Fund your account now, select the deposit money option in the main menu.\n");
-
-				System.out.println("\nPress 0 to return to main menu or # to exit: \n");
-				String userInputForExitOrReturn = input.nextLine();
-			break;
-			default:
-				System.out.println("Invalid input\nTry again.");
-		}
-		System.out.println();
-		System.out.printf("%n%8s Account number %6s Account balance%n%n", "","");
-		return "your accot "customerData[1][0];
 	} 
 }
--- DaySeven/Task1/ATM Machine App/Java/Task7.java
+++ DaySeven/Task1/ATM Machine App/Java/Task7.java
@@ -1,3 +1,4 @@
+import java.security.SecureRandom;
 import java.util.*;
 public class Task7
 {
@@ -22,8 +23,55 @@ public class Task7
 		System.out.print("\nEnter an option: ");
 		int userOption = input.nextInt();
 
-		int accountCreated = Functions.createAccount(userOption);
+		Object[][] customerData = new Object [6][5];
+		double availableBalance = 0.00;
 
-		System.out.print(accountCreated);
+		SecureRandom newUserAccountNumber = new SecureRandom();
+
+		long accountNumber = newUserAccountNumber.nextLong(1000000000, 1111111111);
+		
+		String accountNumberTostring = String.valueOf(accountNumber);
+		String userPinTostring = "";
+
+		int rows = customerData.length;
+		int columns = customerData[0].length;
+		
+
+		double ledgerBalance = 0.00;
+
+		switch(userOption)
+		{
+			case 1:
+				input.nextLine();
+				System.out.print("\nWhat is your name?  ");
+				String customerName = input.nextLine();
+				System.out.print("\nCreate a secret pin:  ");
+				String customerSecretPin = input.nextLine();
+				customerData[0][0] = customerName;
+				customerData[0][1] = accountNumberTostring;
+				customerData[0][2] = availableBalance;
+				customerData[0][3] = customerSecretPin;
+				System.out.print(customerData[0][3]);
+				System.out.println("\nYour account was successfully created.\nYour account number is " + customerData[0][1]);
+				System.out.print("\nYou are required to fund your account within the next 24hrs, or risk termination of your account.\nTo Fund your account now, select the deposit money option in the main menu.\n");
+
+				System.out.println("\nPress 0 to return to main menu or # to exit: \n");
+				String userInputForExitOrReturn = input.nextLine();
+				
+				switch(userInputForExitOrReturn)
+				{
+					case "0":
+						System.out.print(message);
+					case "#":
+						System.exit(0);
+				}
+			break;
+			default:
+				System.out.println("Invalid input\nTry again.");
+		}
+		System.out.println();
+		/*System.out.printf("%n%8s Account number %6s Account balance %6s Pin%n%n", "","","");
+		System.out.printf("%s %5s %s %13s %s %13s %s",customerData[0][0], "", customerData[0][1], "", customerData[0][2], "", customerData[0][3]);
+		System.out.println();*/
 	}
 }
